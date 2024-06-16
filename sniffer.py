8c08342708d24c448c69e48807941f5a700b913e
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-17 00:57:46 +0530

    Added TCP Frame Headers

--- sniffer.py
+++ sniffer.py
@@ -23,6 +23,23 @@ def ip_headers(packet):
     source_address = socket.inet_ntoa(ip_headers_content[12:16])
     destination_address = socket.inet_ntoa(ip_headers_content[16:20])
     return version, ihl, total_length, identification, ttl, protocol, source_address, destination_address, header_length
+def tcp_headers(packet):
+    source_port = int(binascii.hexlify(packet[:2]).decode(), 16)
+    destination_port = int(binascii.hexlify(packet[2:4]).decode(), 16)
+    sequence_number = int(binascii.hexlify(packet[4:8]).decode(), 16)
+    ack_number = int(binascii.hexlify(packet[8:12]).decode(), 16)
+    offset = packet[12] >> 4
+    packet_length = offset * 4
+    data = packet[20+offset:]
+    flags = bin(packet[13])[2:]
+    flags = '0' * (8-len(flags)) + flags
+    urg = flags[2]
+    ack = flags[3]
+    psh = flags[4]
+    rst = flags[5]
+    syn = flags[6]
+    fin = flags[7]
+    return source_port, destination_port, sequence_number, ack_number, urg, ack, psh, rst, syn, fin, packet_length, data
 
 if __name__ == "__main__":
     raw_socket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
@@ -34,6 +51,9 @@ if __name__ == "__main__":
             version, ihl, total_length, identification, ttl, protocol, source_address, destination_address, ip_header_length = ip_headers(packet)
             print(f"\tIPv4 Packet\n\t\tVersion => {version}, Length => {total_length}, TTL => {ttl}\n\t\tProtocol => {protocol}, Source Address => {source_address}, Destination Address => {destination_address}")
             remaining_packet = packet[eth_header_length+ip_header_length: ]
+            if protocol == 6:
+                source_port, destination_port, sequence_number, ack_number, urg, ack, psh, rst, syn, fin, packet_length, data = tcp_headers(remaining_packet)
+                print(f"\tTCP Packet\n\t\tSource Port => {source_port}, Destination Port => {destination_port}\n\t\tSequence Number => {sequence_number}, Acknowledgment Number => {ack_number}\n\t\tFlags\n\t\t\tURG => {urg}, ACK => {ack}, PSH => {psh}\n\t\t\tRST => {rst}, SYN => {syn}, FIN => {fin}")
     except KeyboardInterrupt:
         print("\nKeyboard Interrupt")
     except Exception as error:
