7b9e7b24535efdb7eb17d83375d980eda3b7b82f
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-17 00:57:18 +0530

    Added IP Frame Headers

--- sniffer.py
+++ sniffer.py
@@ -11,6 +11,18 @@ def eth_headers(packet):
     source_mac = MACformat(binascii.hexlify(packet[6:12]).decode())
     eth_type = int(binascii.hexlify(packet[12:14]).decode())
     return destination_mac, source_mac, eth_type
+def ip_headers(packet):
+    ip_headers_content = packet[eth_header_length:]
+    version = ip_headers_content[0] >> 4
+    ihl = ip_headers_content[0] & 0xF
+    header_length = ihl * 4
+    total_length = int(binascii.hexlify(ip_headers_content[2:4]).decode(), 16)
+    identification = int(binascii.hexlify(ip_headers_content[4:6]).decode(), 16)
+    ttl = ip_headers_content[8]
+    protocol = ip_headers_content[9]
+    source_address = socket.inet_ntoa(ip_headers_content[12:16])
+    destination_address = socket.inet_ntoa(ip_headers_content[16:20])
+    return version, ihl, total_length, identification, ttl, protocol, source_address, destination_address, header_length
 
 if __name__ == "__main__":
     raw_socket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
@@ -19,6 +31,9 @@ if __name__ == "__main__":
             packet = raw_socket.recvfrom(65535)[0]
             destination_mac, source_mac, eth_type = eth_headers(packet)
             print(f"Ethernet Frame\nDestination MAC => {destination_mac}, Source MAC => {source_mac}, Protocol => {eth_type}")
+            version, ihl, total_length, identification, ttl, protocol, source_address, destination_address, ip_header_length = ip_headers(packet)
+            print(f"\tIPv4 Packet\n\t\tVersion => {version}, Length => {total_length}, TTL => {ttl}\n\t\tProtocol => {protocol}, Source Address => {source_address}, Destination Address => {destination_address}")
+            remaining_packet = packet[eth_header_length+ip_header_length: ]
     except KeyboardInterrupt:
         print("\nKeyboard Interrupt")
     except Exception as error:
