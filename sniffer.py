6cb0605be4d5a0882dde6e80e1354f9d0eaf7f3b
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-17 00:56:41 +0530

    Created Socket Object and Started Sniffing Packets

--- sniffer.py
+++ sniffer.py
@@ -1,4 +1,13 @@
 #! /usr/bin/env python3
 
+import socket, binascii
+
 if __name__ == "__main__":
-    pass
+    raw_socket = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x0800))
+    try:
+        while True:
+            packet = raw_socket.recvfrom(65535)[0]
+    except KeyboardInterrupt:
+        print("\nKeyboard Interrupt")
+    except Exception as error:
+        print(error)
